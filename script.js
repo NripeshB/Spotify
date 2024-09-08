bc638c2745305267ab868581c4fb3981bfa45b00
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-09-08 09:21:08 +0530

    .

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
 const audioPlayer = document.getElementById("audioPlayer");
 
-// Function to play the song and update active class
+ 
 function playSong(lielement, songsrc) {
-    document.querySelectorAll(".song").forEach(li => li.classList.remove("active")); // Remove active class from all songs
+    document.querySelectorAll(".song").forEach(li => li.classList.remove("active"));
 
-    lielement.classList.add("active"); // Add active class to the clicked song
+    lielement.classList.add("active");
     audioPlayer.src = songsrc;
+    const playIcon = document.getElementById("play");
+        if (audioPlayer.paused) {
+            playIcon.classList.add("fa-pause");
+            playIcon.classList.remove("fa-play");
+            audioPlayer.play();
+        }
     audioPlayer.play();
 
     audioPlayer.onended = function() {
-        lielement.classList.remove('active'); // Remove active class when the song ends
+        lielement.classList.remove('active'); 
     };
 }
 
-// Select all elements with the class 'song' and set up event listeners
-document.querySelectorAll(".song").forEach(item => {
-    item.addEventListener('click', function() {
-        const songSrc = this.getAttribute('data-src'); // Get song source from data attribute
-        playSong(this, songSrc);
+document.querySelectorAll(".player").forEach(item => {
+    item.addEventListener("click", function() {
+        const playIcon = document.getElementById("play");
+        if (audioPlayer.paused) {
+            playIcon.classList.add("fa-pause");
+            playIcon.classList.remove("fa-play");
+            audioPlayer.play();
+        } else {
+            playIcon.classList.remove("fa-pause");
+            playIcon.classList.add("fa-play");
+            audioPlayer.pause();
+        }
     });
 });
 
-// Select all 'i' elements within .items and set up event listeners
+ 
+document.querySelectorAll(".song").forEach(item => {
+    item.addEventListener('click', function() {
+        const songSrc = this.getAttribute('data-src');  
+        playSong(this, songSrc);
+    });
+});
+ 
 document.querySelectorAll(".Creators .fa-play").forEach(playIcon => {
     playIcon.addEventListener('click', function() {
-        const songSrc = this.getAttribute('data-src'); // Get song source from data attribute
+        const songSrc = this.getAttribute('data-src');   
         if (songSrc) {
             playSong(document.querySelector(`.song[data-src="${songSrc}"]`), songSrc);
         }
